05d9c83daf93ff9c71097c6375e170e717aabebe
Stephen Mark <stephenjmark@gmail.com>
2017-11-07 03:14:43 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -9,7 +9,7 @@ function teenager(age) {
   if (age >= 13 && age <= 19 ) {
   return 'You are a teenager!' // executed if `conditionToTest` is truthy
   } else {
-  return 'You are not a teenager!' // executed if `conditionToTest` is falsey
+  return 'You are not a teenager' // executed if `conditionToTest` is falsey
   }
 }
 
