af3d92608f8c81f075ce9a6bd9f707d2f09e801b
Stephen Mark <stephenjmark@gmail.com>
2017-11-07 03:10:56 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,7 @@
 function basicTeenager(age) {
-  if (age > 13 && age < 19) {
-    return 'You are a teenager!'
+  if (13 < age && age < 19) {
+  // do something
+  return 'You are a teenager!'
   }
 }
 
